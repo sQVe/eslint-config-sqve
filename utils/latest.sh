c3bb2e6f951dbb72b96132809fcca3de384025c3
Oskar Grunning <oskargrunning@gmail.com>
2018-08-20 22:49:28 +0200

    Update dependencies

--- utils/latest.sh
+++ utils/latest.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 #
-#  ╦  ╔═╗╔╦╗╔═╗╔═╗╔╦╗ 
-#  ║  ╠═╣ ║ ║╣ ╚═╗ ║  
-#  ╩═╝╩ ╩ ╩ ╚═╝╚═╝ ╩  
+#  ╦  ╔═╗╔╦╗╔═╗╔═╗╔╦╗
+#  ║  ╠═╣ ║ ║╣ ╚═╗ ║
+#  ╩═╝╩ ╩ ╩ ╚═╝╚═╝ ╩
 #
 # Help util to add / upgrade dependencies to the latest version.
 
 dependencies=(
-  "eslint@latest" 
-  "babel-eslint@latest" 
+  "eslint@latest"
+  "babel-eslint@latest"
   "eslint-config-prettier@latest"
 )
 airbnbConfig="eslint-config-airbnb"
 
 # Add/Update non airbnb dependencies
-yarn add "${dependencies[@]}"
+npm install "${dependencies[@]}"
 
 # Add/Update airbnb eslint config and it's peerDependencies
-npm info "$airbnbConfig@latest" peerDependencies --json | command sed 's/[\{\},]//g ; s/: /@/g' | xargs yarn add "$airbnbConfig@latest" --dev
+npm info "$airbnbConfig@latest" peerDependencies --json | command sed 's/[\{\},]//g ; s/: /@/g' | xargs npm install "$airbnbConfig@latest" --save-dev
