2ac5c4166905e95d9213c3145af537fbde0e8e15
Oskar Grunning <oskargrunning@gmail.com>
2019-04-13 23:51:03 +0200

    Remove legacy and unnecessary JsDoc rules

--- index.js
+++ index.js
@@ -1,28 +1,7 @@
-const rules = {
-  'valid-jsdoc': [
-    'error',
-    {
-      prefer: {
-        arg: 'param',
-        argument: 'param',
-        constructor: 'class',
-        return: 'returns',
-      },
-      preferType: {
-        Boolean: 'boolean',
-        Number: 'number',
-        Object: 'object',
-        String: 'string',
-      },
-    },
-  ],
-}
-
 module.exports = {
   parser: 'babel-eslint',
   extends: ['standard', 'standard-react', 'prettier', 'prettier/react'],
   env: { browser: true, es6: true, node: true },
-  rules,
   overrides: [
     {
       files: ['test/**/*.{js,mjs}', '**/*.test.{js,mjs}'],
