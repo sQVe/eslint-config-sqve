47f150f4fd8e0f33313038286e05268465ef6565
Oskar Grunning <oskargrunning@gmail.com>
2019-04-13 23:33:11 +0200

    Remove required tags from JsDoc

--- index.js
+++ index.js
@@ -14,11 +14,6 @@ const rules = {
         Object: 'object',
         String: 'string',
       },
-      requireParamDescription: false,
-      requireReturnDescription: false,
-      requireParamType: true,
-      requireReturn: true,
-      requireReturnType: true,
     },
   ],
 }
