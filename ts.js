9202b2db7bb5bef0ab6cd49d5c1e6fc274b24e12
Oskar Grunning <oskargrunning@gmail.com>
2019-09-04 09:20:24 +0200

    Add parserOptions to TypeScript configs

--- ts.js
+++ ts.js
@@ -1,5 +1,11 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: { jsx: true },
+    useJSXTextNode: true,
+    project: './tsconfig.json',
+    tsconfigRootDir: '../../',
+  },
   extends: [
     './index.js',
     'plugin:@typescript-eslint/recommended',
