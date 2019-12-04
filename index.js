9ce6aff3aba1edd558532ba0c331188781540f89
Oskar Grunning <oskargrunning@gmail.com>
2019-12-04 14:04:20 +0100

    feat(type-checking): disable type-checking dependent rules

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ const typescriptSettings = {
   files: ['**/*.{ts,tsx}'],
   extends: [
     'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'prettier/@typescript-eslint',
   ],
   parser: '@typescript-eslint/parser',
