c44d12200340d7028b7277086181fe313c37e688
Oskar Grunning <oskargrunning@gmail.com>
2020-09-08 14:38:57 +0200

    fix(typescript): disable explicit module boundary rule

--- index.js
+++ index.js
@@ -89,6 +89,7 @@ const typescript = {
     ...common.rules,
     'no-unused-vars': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-unused-vars': [
       'error',
       {
