d0d7191422c327ea1f9e48a172669ed99148be9f
Oskar Grunning <oskargrunning@gmail.com>
2020-10-28 13:30:07 +0100

    feat(next): add next config

--- index.js
+++ index.js
@@ -23,12 +23,6 @@ const common = {
   parser: 'babel-eslint',
   rules: {
     /**
-     * Disable enforcing valid anchor.
-     * https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/master/docs/rules/anchor-is-valid.md
-     */
-    'jsx-a11y/anchor-is-valid': 'off',
-
-    /**
      * Disallow empty functions.
      * https://eslint.org/docs/rules/no-empty-function
      */
---
+++ next.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ['plugin:@next/next:recommended'],
+  rules: {
+    /**
+     * Disable enforcing valid anchor.
+     * https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/master/docs/rules/anchor-is-valid.md
+     */
+    'jsx-a11y/anchor-is-valid': 'off',
+  },
+};
